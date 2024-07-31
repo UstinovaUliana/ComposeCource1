7c46c6ed8be1cd95e63f4c8cecad51841f26b809
Uliana <mirrorrune21363@gmail.com>
2024-07-31 13:03:37 +0300

    create states project

---
+++ StateInCompose/app/src/main/java/com/ustinovauliana/stateincompose/WellnessScreen.kt
@@ -0,0 +1,34 @@
+package com.ustinovauliana.stateincompose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    var count = 0
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "You've had $count glasses.",
+            modifier = modifier.padding(16.dp)
+        )
+        Button(
+            onClick = {
+                count++
+            },
+            Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
+    }
+
+}
