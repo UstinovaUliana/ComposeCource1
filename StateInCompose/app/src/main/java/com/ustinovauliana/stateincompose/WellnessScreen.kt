f7041061af38e2a3b5405bb8445dc90bc6c2fe9e
Uliana <mirrorrune21363@gmail.com>
2024-07-31 20:25:42 +0300

    finish states in compose

--- StateInCompose/app/src/main/java/com/ustinovauliana/stateincompose/WellnessScreen.kt
+++ StateInCompose/app/src/main/java/com/ustinovauliana/stateincompose/WellnessScreen.kt
@@ -5,30 +5,43 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+fun WellnessScreen(modifier: Modifier = Modifier,
+                   wellnessViewModel: WellnessViewModel = viewModel()) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList(list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task -> wellnessViewModel.remove(task) })
+    }
 }
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+fun StateLessCounter(count: Int, onIncrement:() -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        Text(
-            text = "You've had $count glasses.",
-            modifier = modifier.padding(16.dp)
-        )
-        Button(
-            onClick = {
-                count++
-            },
-            Modifier.padding(top = 8.dp)
-        ) {
-            Text(text = "Add one")
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
+}
 
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StateLessCounter(count, {count++}, modifier)
 }
