03a73df44bdd83d09b194e85227fee578df03a16
Uliana <mirrorrune21363@gmail.com>
2024-08-02 14:53:12 +0300

    start compose m3 theming course

---
+++ ComposeM3ThemingApp/app/src/main/java/com/ustinovauliana/composem3themingapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ustinovauliana.composem3themingapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ustinovauliana.composem3themingapp.ui.theme.ComposeM3ThemingAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeM3ThemingAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeM3ThemingAppTheme {
+        Greeting("Android")
+    }
+}
