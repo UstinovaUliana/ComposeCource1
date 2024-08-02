cc44ae10487b9feee6aab6d6e21d6bc864fdf465
Uliana <mirrorrune21363@gmail.com>
2024-08-02 20:29:31 +0300

    finish theming course

--- ComposeM3ThemingApp/app/src/main/java/com/ustinovauliana/composem3themingapp/ui/components/ReplyAppBars.kt
+++ ComposeM3ThemingApp/app/src/main/java/com/ustinovauliana/composem3themingapp/ui/components/ReplyAppBars.kt
@@ -1,28 +1,14 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.ustinovauliana.composem3themingapp.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -46,7 +32,11 @@ fun ReplySearchBar(modifier: Modifier = Modifier) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(16.dp)
+            .background(
+                MaterialTheme.colorScheme.background,
+                CircleShape
+            ),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
@@ -111,7 +101,7 @@ fun EmailDetailAppBar(
                     )
                 ) {
                     Icon(
-                        imageVector = Icons.Default.ArrowBack,
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = stringResource(id = R.string.back_button),
                         modifier = Modifier.size(14.dp)
                     )
--- ComposeM3ThemingApp/app/src/main/java/com/ustinovauliana/composem3themingapp/ui/theme/Color.kt
+++ ComposeM3ThemingApp/app/src/main/java/com/ustinovauliana/composem3themingapp/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.compose
+package com.ustinovauliana.composem3themingapp.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val primaryLight = Color(0xFF8D4E2C)
