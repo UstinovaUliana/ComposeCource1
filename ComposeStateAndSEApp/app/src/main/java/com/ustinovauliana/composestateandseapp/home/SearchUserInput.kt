6246b564445be3b01a3f9ae2f42c852d6702ca55
Uliana <mirrorrune21363@gmail.com>
2024-08-06 19:57:12 +0300

    finish (DisposableEffect, produceState, and derivedStateOf)

--- ComposeStateAndSEApp/app/src/main/java/com/ustinovauliana/composestateandseapp/home/SearchUserInput.kt
+++ ComposeStateAndSEApp/app/src/main/java/com/ustinovauliana/composestateandseapp/home/SearchUserInput.kt
@@ -24,11 +24,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import com.ustinovauliana.composestateandseapp.R
 import com.ustinovauliana.composestateandseapp.base.CraneEditableUserInput
 import com.ustinovauliana.composestateandseapp.base.CraneUserInput
@@ -37,6 +40,9 @@ import com.ustinovauliana.composestateandseapp.home.PeopleUserInputAnimationStat
 import com.ustinovauliana.composestateandseapp.ui.CraneTheme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.ustinovauliana.composestateandseapp.base.EditableUserInputState
+import com.ustinovauliana.composestateandseapp.base.rememberEditableUserInputState
+import kotlinx.coroutines.flow.filter
 
 enum class PeopleUserInputAnimationState { Valid, Invalid }
 
@@ -97,12 +103,21 @@ fun FromDestination() {
 
 @Composable
 fun ToDestinationUserInput(onToDestinationChanged: (String) -> Unit) {
+    val editableUserInputState = rememberEditableUserInputState(hint = "Choose Destination")
     CraneEditableUserInput(
-        hint = "Choose Destination",
+        state = editableUserInputState,
         caption = "To",
         vectorImageId = R.drawable.ic_plane,
-        onInputChanged = onToDestinationChanged
     )
+
+    val currentOnDestinationChanged by rememberUpdatedState(onToDestinationChanged)
+    LaunchedEffect(editableUserInputState) {
+        snapshotFlow { editableUserInputState.text }
+            .filter { !editableUserInputState.isHint }
+            .collect {
+                currentOnDestinationChanged(editableUserInputState.text)
+            }
+    }
 }
 
 @Composable
